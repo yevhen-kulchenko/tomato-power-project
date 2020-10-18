2cc77e3e227ae6b67c7089f04b305de2dd54889e
yevhen kulchenko <evhenkulchenko@gmail.com>
2020-10-18 17:42:19 +0300

    Updates

--- menu.234110a1.js
+++ menu.234110a1.js
@@ -156,7 +156,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59432" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52651" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
